858df49408b72feec506c39b18dda9cfba5ee0fd
mrami <si.mohamedrami1902@gmail.com>
2024-02-03 14:07:25 +0100

    create Navbar && dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
-import Announce from "./components/Announcement/Announce";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./pages/Login/Login";
+import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
-  return (
-	<div>
-		<Announce/>
-		<Navbar/>
-	</div>
-  )
+	return (
+		<div>
+			<Navbar/>
+		</div>
+	)
 }
 
-export default App;
+export default App
